03495a1971dc501615f9c34f6ae968ddfc2dd3a9
Lacey Washington <lacey.m.washington@gmail.com>
2023-03-18 21:11:08 -0500

    reorganized code and added local storage for site need to work on CSS on monday.

--- JS/app.js
+++ JS/app.js
@@ -2,66 +2,16 @@
 // Global variables
 let voteCount = 25;
 
-// DOM reference 
-const imgHolder = document.getElementById('image-selection');
-const imgOne = document.getElementById('img-one');
-const imgTwo = document.getElementById('img-two');
-const imgThree = document.getElementById('img-three');
-const viewResults = document.getElementById('results-button');
-const list = document.getElementById('list-results');
-const ctx = document.getElementById('results-chart');
-
 // object that holds products
 const state = {
   allTheProducts: []
 
 };
-console.log(state);
 
-// decided not to do a bubble cuz that for caught up Lacey so I'm gonna just do a bar and try to do the bubble next night.
-function renderChart() {
-  ctx.style.display = 'block';
-  let productNames = [];
-  let productVotes = [];
-  let productViews = [];
-
-  for (let i = 0; i < state.allTheProducts.length; i++) {
-    productNames.push(state.allTheProducts[i].name);
-    productVotes.push(state.allTheProducts[i].votes);
-    productViews.push(state.allTheProducts[i].views);
-  }
-
-
-  let results = {
-    type: 'bar',
-    data: {
-      labels: productNames,
-      datasets: [{
-        label: '# of Votes',
-        data: productVotes,
-        borderWidth: 3,
-        backgroundColor: '#FFFF00'
-      },
-      {
-        label: '# of views',
-        data: productViews,
-        borderWidth: 2,
-        backgroundColor: '#00FF00'
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  };
-
-  new Chart(ctx, results);
-
+//helper function
+function getRandomPic() {
+  return Math.floor(Math.random() * state.allTheProducts.length);
 }
-
 // constructor Function for products
 function OddDuckProduct(name, fileExtension = 'jpg') {
   this.name = name;
@@ -72,42 +22,57 @@ function OddDuckProduct(name, fileExtension = 'jpg') {
   state.allTheProducts.push(this);
 }
 
+//Retrieving local Data
+let retrievedData = localStorage.getItem('products');
+let parsedData = JSON.parse(retrievedData);
 
-// photos for Products
-const bag = new OddDuckProduct('bag');
-const banana = new OddDuckProduct('banana');
-const bathroom = new OddDuckProduct('bathroom');
-const boots = new OddDuckProduct('boots');
-const breakfast = new OddDuckProduct('breakfast');
-const bubblegumMeat = new OddDuckProduct('bubblegum');
-const chair = new OddDuckProduct('chair');
-const OurDarkLord = new OddDuckProduct('cthulhu');
-const doggoDucky = new OddDuckProduct('dog-duck');
-const dragon = new OddDuckProduct('dragon');
-const penForks = new OddDuckProduct('pen');
-const petSweeper = new OddDuckProduct('pet-sweep');
-const pizzaScissors = new OddDuckProduct('scissors');
-const jawsSleepingBag = new OddDuckProduct('shark');
-const babySweeper = new OddDuckProduct('sweep');
-const starWarsSleepingBag = new OddDuckProduct('tauntaun');
-const unicornMeat = new OddDuckProduct('unicorn');
-const uselessWaterCan = new OddDuckProduct('water-can');
-const interestingWineGlass = new OddDuckProduct('wine-glass');
+if (retrievedData) {
+  state.allTheProducts = parsedData;
+  voteCount = 25;
+} else {
+  // photos for Products
+  const bag = new OddDuckProduct('bag');
+  const banana = new OddDuckProduct('banana');
+  const bathroom = new OddDuckProduct('bathroom');
+  const boots = new OddDuckProduct('boots');
+  const breakfast = new OddDuckProduct('breakfast');
+  const bubblegumMeat = new OddDuckProduct('bubblegum');
+  const chair = new OddDuckProduct('chair');
+  const OurDarkLord = new OddDuckProduct('cthulhu');
+  const doggoDucky = new OddDuckProduct('dog-duck');
+  const dragon = new OddDuckProduct('dragon');
+  const penForks = new OddDuckProduct('pen');
+  const petSweeper = new OddDuckProduct('pet-sweep');
+  const pizzaScissors = new OddDuckProduct('scissors');
+  const jawsSleepingBag = new OddDuckProduct('shark');
+  const babySweeper = new OddDuckProduct('sweep');
+  const starWarsSleepingBag = new OddDuckProduct('tauntaun');
+  const unicornMeat = new OddDuckProduct('unicorn');
+  const uselessWaterCan = new OddDuckProduct('water-can');
+  const interestingWineGlass = new OddDuckProduct('wine-glass');
 
-
-//helper function
-function getRandomPic() {
-  return Math.floor(Math.random() * state.allTheProducts.length);
 }
+
+// DOM reference 
+const imgHolder = document.getElementById('image-selection');
+const imgOne = document.getElementById('img-one');
+const imgTwo = document.getElementById('img-two');
+const imgThree = document.getElementById('img-three');
+const viewResults = document.getElementById('results-button');
+const list = document.getElementById('list-results');
+const ctx = document.getElementById('results-chart');
+
+
+
 
 // Renders images for clicks without having continuous repeats
 function renderImgs() {
   let picArray = [];
   while (picArray.length < 3) {
     let newIndex = getRandomPic();
-    if(picArray.indexOf(newIndex) === -1) {
+    if (picArray.indexOf(newIndex) === -1) {
       picArray.push(newIndex);
-    }else{
+    } else {
       newIndex = getRandomPic();
     }
   }
@@ -118,39 +83,39 @@ function renderImgs() {
   imgOne.src = state.allTheProducts[indexOne].photo;
   imgOne.alt = state.allTheProducts[indexOne].name;
   ++state.allTheProducts[indexOne].views;
-  console.log(state.allTheProducts[indexOne].views);
 
   imgTwo.src = state.allTheProducts[indexTwo].photo;
   imgTwo.alt = state.allTheProducts[indexTwo].name;
   ++state.allTheProducts[indexTwo].views;
-  console.log(state.allTheProducts[indexTwo].views);
 
   imgThree.src = state.allTheProducts[indexThree].photo;
   imgThree.alt = state.allTheProducts[indexThree].name;
   ++state.allTheProducts[indexThree].views;
-  console.log(state.allTheProducts[indexThree].views);
 }
 
 
-
+//Click event
 function handleClick(event) {
   --voteCount;
-  console.log(voteCount);
   let imgClicked = event.target.alt;
   for (let i = 0; i < state.allTheProducts.length; i++) {
     if (imgClicked === state.allTheProducts[i].name) {
       ++state.allTheProducts[i].votes;
-      console.log(imgClicked, state.allTheProducts[i].votes);
     }
   }
+
   renderImgs();
 
   if (voteCount === 0) {
     imgHolder.removeEventListener('click', handleClick);
+    // LocalStorage begins
+    let stringifiedProducts = JSON.stringify(state.allTheProducts);
+    // Store the data.
+    localStorage.setItem('products', stringifiedProducts);
   }
   console.log(voteCount);
 }
-
+// renders List
 function handleViewResults() {
   if (voteCount === 0) {
     for (let i = 0; i < state.allTheProducts.length; i++) {
@@ -159,10 +124,53 @@ function handleViewResults() {
       list.append(liElm);
     }
   }
-  // rendering chart
+  // decided not to do a bubble cuz that for caught up Lacey so I'm gonna just do a bar and try to do the bubble next night.
+  function renderChart() {
+    ctx.style.display = 'block';
+    let productNames = [];
+    let productVotes = [];
+    let productViews = [];
+
+    for (let i = 0; i < state.allTheProducts.length; i++) {
+      productNames.push(state.allTheProducts[i].name);
+      productVotes.push(state.allTheProducts[i].votes);
+      productViews.push(state.allTheProducts[i].views);
+    }
+
+
+    let results = {
+      type: 'bar',
+      data: {
+        labels: productNames,
+        datasets: [{
+          label: '# of Votes',
+          data: productVotes,
+          borderWidth: 3,
+          backgroundColor: '#FFFF00'
+        },
+        {
+          label: '# of views',
+          data: productViews,
+          borderWidth: 2,
+          backgroundColor: '#00FF00'
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+
+    new Chart(ctx, results);
+
+  }
+  // rendering chart in handleClick()
   viewResults.style.display = 'none';
   renderChart();
-
+  console.log(state);
 }
 
 // listeners
@@ -172,6 +180,7 @@ imgHolder.addEventListener('click', handleClick);
 
 // function for rendering imgs invocation
 renderImgs();
+console.log(state);
 
 
 
